383075d2dddcb031e301a04997c61249b381442a
JoshuaBrest <36625023+JoshuaBrest@users.noreply.github.com>
2023-09-07 22:43:41 -0700

    disable devtools in prod?

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+// import devtools from 'solid-devtools/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
-        devtools(),
+        // devtools(),
         solidPlugin(),
         VitePWA({
             manifest: {
