2a42f4a9bc3fe43df31326cbc24faf06a9674162
JoshuaBrest <36625023+JoshuaBrest@users.noreply.github.com>
2023-09-07 22:19:20 -0700

    Make locales work

--- src/locales/index.tsx
+++ src/locales/index.tsx
@@ -1,10 +1,27 @@
 import { I18nContext, createI18nContext } from '@solid-primitives/i18n';
 import type { FlowComponent } from 'solid-js';
+import enGb from './en-gb.json';
 import enUs from './en-us.json';
+import esEs from './es-es.json';
+import frFr from './fr-fr.json';
+import jaJP from './ja-jp.json';
+import zhCn from './zh-cn.json';
+import zhTw from './zh-tw.json';
 
-const ctx = createI18nContext({ enUs }, 'en-us');
+const ctx = createI18nContext(
+    {
+        enGb,
+        enUs,
+        esEs,
+        frFr,
+        jaJP,
+        zhCn,
+        zhTw
+    },
+    'en-us'
+);
 
-const availableLocales = ['en-us'];
+const availableLocales = ['en-gb', 'en-us', 'es-es', 'fr-fr', 'ja-jp', 'zh-cn', 'zh-tw'] as const;
 
 const I18nProvider: FlowComponent<{
     dict?: Record<string, Record<string, any>>;
@@ -13,4 +30,4 @@ const I18nProvider: FlowComponent<{
     return <I18nContext.Provider value={ctx}>{props.children}</I18nContext.Provider>;
 };
 
-export { en, I18nProvider, availableLocales };
+export { enUs, I18nProvider, availableLocales };
