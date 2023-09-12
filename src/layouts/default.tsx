ebd81538f70dbe64c4e5204ec716765bf9f5e263
JoshuaBrest <36625023+JoshuaBrest@users.noreply.github.com>
2023-09-11 22:14:15 -0700

    Fix stuff and add robots.txt

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -10,7 +10,7 @@ export const DefaultLayout: Component<DefaultLayoutProps> = (props: DefaultLayou
         <div class='flex min-h-full w-full flex-row text-white'>
             <SideBar />
             <div class='flex min-h-full w-full justify-center'>
-                <div class='flex min-h-full w-full max-w-4xl flex-col'>{props.children}</div>
+                <div class='flex min-h-full w-full max-w-4xl flex-1 flex-col'>{props.children}</div>
             </div>
             <BottomBar />
         </div>
